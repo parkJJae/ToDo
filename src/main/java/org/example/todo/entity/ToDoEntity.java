14af5168a2889cf77cb50d890bd6e5ecdec33678
parkJJae <wgpark0830@naver.com>
2025-02-10 22:27:19 +0900

    엔티티

---
+++ src/main/java/org/example/todo/entity/ToDoEntity.java
@@ -0,0 +1,24 @@
+package org.example.todo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity // 데이터베이스 테이블과 연결됨
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ToDoEntity {
+    @Id // 기본 키
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동 증가 ID
+    private int id;
+
+    private String title;
+    private boolean completed;
+}
