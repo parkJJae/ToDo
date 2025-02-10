3c38fc6958e3e90bfa47ab3072f20a441eb5b90d
parkJJae <wgpark0830@naver.com>
2025-02-10 22:27:12 +0900

    컨트롤러

---
+++ src/main/java/org/example/todo/controller/TodoController.java
@@ -0,0 +1,32 @@
+package org.example.todo.controller;
+
+import org.example.todo.entity.ToDoEntity;
+import org.example.todo.service.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService){
+        this.todoService = todoService;
+    }
+
+    @GetMapping
+    public List<ToDoEntity> getAllTodos() {
+        return todoService.getAllTodos();
+    }
+
+    @PostMapping
+    public ToDoEntity createTodo(@RequestBody ToDoEntity todoentity){
+        return todoService.createTodo(todoentity);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTodo(@PathVariable Long id){
+        todoService.deleteTodo(id);
+    }
+}
